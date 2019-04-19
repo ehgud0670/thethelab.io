4e3f7de7add70cafd62347c536ca461409bb7f6f
ehgud0670 <kimdo2297@gmail.com>
2019-04-19 11:18:58 +0900

    kimdo

--- homework/kimdo/thread/Project.java
+++ homework/kimdo/thread/Project.java
@@ -6,18 +6,14 @@ public class Project {
 		if (args.length == 1) {
 			thread_count = Integer.parseInt(args[0]);
 		}
-		Thread[] threads = new Fthread[thread_count];
+		Thread[] threads = new AddThread[thread_count];
 
 		long start = System.currentTimeMillis();
-
-
 		//멀티 스레드 동작
 		for (int i = 0; i < thread_count; i++) {
-			threads[i] = new Fthread();
+			threads[i] = new AddThread();
 			threads[i].start();
 		}
-
-
 
 		//메인에서 대기
 		for (int i = 0; i < thread_count; i++) {
@@ -29,32 +25,27 @@ public class Project {
 		}
 
 		System.out.println("sum: " + sum);
-
-
 		long end = System.currentTimeMillis();
-		System.out.println("time: " + (end-start)/1000.0 );
-
+		System.out.println("time: " + (end-start)/1000.0);
 	}
 
-	static void foo() {
+	static class AddThread extends Thread {
 
-		long local = 0;
-		for (long i = 0; i < 2100000000 / thread_count; i++) {
-			++local;
-		}
-		synchronized (Project.class) {
-			sum += local;
-		}
+		@Override
+			public void run() {
+				add();
+			}
 
+		static void add() {
+
+			long local = 0;
+			for (long i = 0; i < 2100000000 / thread_count; i++) {
+				++local;
+			}
+			synchronized (AddThread.class) {
+				sum += local;
+			}
+
+		}
 	}
-}
-
-
-class Fthread extends Thread{
-
-	@Override
-		public void run() {
-			Project.foo();
-		}
-
 }
